fbd17cd7b95a942709422044c142ad727b72a005
Ngugi Isaac Kinyanjui <itsngugiisaackinyanjui@gmail.com>
2024-01-12 10:35:21 +0300

    Q1, Upload

---
+++ Lambda/Q1.py
@@ -0,0 +1,19 @@
+# Write a lambda function to add two numbers, and then use it to find the sum of 5 and 7.
+
+sum = lambda x,y: x+y
+print(sum(5,7))
+
+# Create a lambda function to subtract two numbers, and then use it to find the difference between 10 and 3.
+
+sub = lambda x,y: x-y
+print(sub(10,3))
+
+# Define a lambda function for multiplication and use it to calculate the product of 8 and 6.
+
+multiply = lambda x,y: x*y
+print(multiply(8,6))
+
+# Write a lambda function for division and use it to find the quotient of 15 divided by 3.
+
+division = lambda x,y: x/y
+print(division(15,3))
