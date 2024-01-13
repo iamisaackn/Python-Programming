3b670e8e302594fa64a18ea3b809638051485413
Ngugi Isaac Kinyanjui <itsngugiisaackinyanjui@gmail.com>
2024-01-13 16:11:25 +0300

    Upload Arithmetic Sequence

---
+++ Mathematics/Arithmetic Sequence/Easy.py
@@ -0,0 +1,6 @@
+# ------------------------ EASY -------------------------
+'''
+Calculate the nth term of an arithmetic sequence:
+Write a Python function that takes the first term (a1), common difference (d), 
+and the term number (n) as input and returns the nth term of the arithmetic sequence.
+'''
