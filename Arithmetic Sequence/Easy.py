e4b2a7ba0fc6752ac22996cd7c7566ff2f569b4c
Ngugi Isaac Kinyanjui <itsngugiisaackinyanjui@gmail.com>
2024-01-16 02:47:12 +0300

    Update

--- Arithmetic Sequence/Easy.py
+++ Arithmetic Sequence/Easy.py
@@ -1,10 +1,14 @@
 # ------------------------ EASY -------------------------
+
+
 '''
+Q1).
+
 Calculate the nth term of an arithmetic sequence:
 Write a Python function that takes the first term (a1), common difference (d), 
 and the term number (n) as input and returns the nth term of the arithmetic sequence.
 '''
-
+'''
 class MySequence:
     def __init__(self, x, y, z):
         self.x = x
@@ -16,11 +20,33 @@ class MySequence:
         print(f"The nth term is: {self.b}")
 
 
-x = int(input("Enter the first term: ")) 
-y = int(input("Enter the common difference: "))
-z = int(input("Enter the term number: "))  
+a1 = int(input("Enter the first term: ")) 
+d = int(input("Enter the common difference: "))
+n = int(input("Enter the term number: "))  
 
-sequence = MySequence( x, y, z)
+sequence = MySequence( a1, d, n)
 
 sequence.my_calc()
+'''
 
+'''
+Q2).
+
+Sum of the first n terms in an arithmetic sequence:
+Write a Python function that calculates the sum of the first n terms in an arithmetic sequence given the first term (a1),
+common difference (d), and the term number (n).
+'''
+
+# first term = a1
+# common difference = d
+# term number = n
+
+def myFunction(a1, d, n):
+    return n/2 * (2*(a1)+(n-1)*d)
+
+a1 = int(input("First Term: "))
+d = int(input("Common Difference: "))
+n = int(input("Term Number: "))
+
+result = myFunction(a1, d, n)
+print(f"The sum of the {n} first term is {result}")
